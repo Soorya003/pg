2b2f5e29958702ebb328221e88daa9437334ff7d
Soorya003 <51847100+Soorya003@users.noreply.github.com>
2024-05-23 21:11:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+var pagination =document.createElement("div");
+pagination.className="pagination";
+
+var a1=document.createElement("a");
+a1.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a1.innerHTML=1;
+
+var a2=document.createElement("a");
+a2.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a2.innerHTML=2;
+
+var a3=document.createElement("a");
+a3.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a3.innerHTML=3;
+
+var a4=document.createElement("a");
+a4.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a4.innerHTML=4;
+
+var a5=document.createElement("a");
+a5.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a5.innerHTML=5;
+
+var a6=document.createElement("a");
+a6.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a6.innerHTML=6;
+
+var a7=document.createElement("a");
+a7.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a7.innerHTML=7;
+
+var a8=document.createElement("a");
+a8.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a8.innerHTML=8;
+
+var a9=document.createElement("a");
+a9.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a9.innerHTML=9;
+
+var a10=document.createElement("a");
+a10.setAttribute("href","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
+a10.innerHTML=10;
+
+pagination.append(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10);
+document.body.append(pagination);
